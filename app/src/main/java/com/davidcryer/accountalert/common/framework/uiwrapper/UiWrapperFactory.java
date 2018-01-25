399babd258d02e17349eae39bb477b1ce6768511
David Cryer <davecryer.dc@gmail.com>
2018-01-25 23:53:49 +0000

    get latest version of UiWrapper library and use helper method in UiWrapperFactory

--- app/src/main/java/com/davidcryer/accountalert/common/framework/uiwrapper/UiWrapperFactory.java
+++ app/src/main/java/com/davidcryer/accountalert/common/framework/uiwrapper/UiWrapperFactory.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.davidc.uiwrapper.UiWrapper;
+import com.davidc.uiwrapper.UiWrapperInitializer;
 import com.davidcryer.accountalert.accountlist.AccountListUiModelImpl;
 import com.davidcryer.accountalert.accountlist.AccountListUiWrapper;
 import com.davidcryer.accountalert.addaccount.AddAccountUiModelImpl;
@@ -22,7 +22,7 @@ public class UiWrapperFactory {
     }
 
     public AccountListUiWrapper accountList(@Nullable final Bundle savedState) {
-        return wrapper(
+        return UiWrapperInitializer.from(
                 savedState,
                 () -> AccountListUiWrapper.newInstance(new AccountListUiModelImpl(), getAccountsInteractor),
                 nonNullSavedState -> AccountListUiWrapper.savedInstance(nonNullSavedState, getAccountsInteractor)
@@ -30,28 +30,10 @@ public class UiWrapperFactory {
     }
 
     public AddAccountUiWrapper addAccount(@Nullable final Bundle savedState) {
-        return wrapper(
+        return UiWrapperInitializer.from(
                 savedState,
                 () -> AddAccountUiWrapper.newInstance(new AddAccountUiModelImpl(), addAccountInteractor),
                 nonNullSavedState -> AddAccountUiWrapper.savedInstance(nonNullSavedState, addAccountInteractor)
         );
-    }
-
-    private static <T extends UiWrapper> T wrapper(@Nullable final Bundle savedState, final NewWrapperProvider<T> newWrapperProvider, final SavedWrapperProvider<T> savedWrapperProvider) {
-        if (savedState != null) {
-            final T savedWrapper = savedWrapperProvider.from(savedState);
-            if (savedWrapper != null) {
-                return savedWrapper;
-            }
-        }
-        return newWrapperProvider.get();
-    }
-
-    private interface NewWrapperProvider<T extends UiWrapper> {
-        T get();
-    }
-
-    private interface SavedWrapperProvider<T extends UiWrapper> {
-        T from(@NonNull Bundle savedState);
     }
 }
